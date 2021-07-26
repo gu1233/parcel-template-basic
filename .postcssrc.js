b5c7347acdcb0cc2d036ad788f1323234c626a89
gu1233 <gde102@naver.com>
2021-07-26 12:05:23 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,21 @@
+// ESM
+// CommonJS
+
+//import autoprefixer from 'auroprefixer'
+// const autoprefixer = require('auotprefixer')
+
+// export {
+//  plugins : [
+//    autoprefixer
+//  ]
+// }
+// module.exports = {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
